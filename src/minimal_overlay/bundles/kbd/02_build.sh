5dee1b341c5cd882b6298b5cc26ddda8a95ccd7d
mm <mickael.mathieu@kinitic.ch>
2019-07-23 21:02:26 +0200

    reformat vars kbd

--- src/minimal_overlay/bundles/kbd/02_build.sh
+++ src/minimal_overlay/bundles/kbd/02_build.sh
@@ -4,19 +4,19 @@ set -e
 
 . ../../common.sh
 
-cd $WORK_DIR/overlay/$BUNDLE_NAME
+cd ${WORK_DIR}/overlay/${BUNDLE_NAME}
 
 # Change to the kbd source directory which ls finds, e.g. 'kbd-2.04'.
 cd $(ls -d kbd-*)
 
-if [ -f Makefile ] ; then
+if [[ -f Makefile ]] ; then
   echo "Preparing '$BUNDLE_NAME' work area. This may take a while."
-  make -j $NUM_JOBS clean
+  make -j ${NUM_JOBS} clean
 else
   echo "The clean phase for '$BUNDLE_NAME' has been skipped."
 fi
 
-rm -rf $DEST_DIR
+rm -rf ${DEST_DIR}
 
 echo "Configuring '$BUNDLE_NAME'."
 CFLAGS="$CFLAGS" ./configure \
@@ -25,21 +25,21 @@ CFLAGS="$CFLAGS" ./configure \
 # vlock requires PAM
 
 echo "Building '$BUNDLE_NAME'."
-make -j $NUM_JOBS
+make -j ${NUM_JOBS}
 
 echo "Installing '$BUNDLE_NAME'."
-make -j $NUM_JOBS install DESTDIR="$DEST_DIR"
+make -j ${NUM_JOBS} install DESTDIR="$DEST_DIR"
 
 echo "Reducing '$BUNDLE_NAME' size."
 set +e
 strip -g \
-  $DEST_DIR/usr/bin/* \
-  $DEST_DIR/usr/sbin/* \
-  $DEST_DIR/lib/*
+  ${DEST_DIR}/usr/bin/* \
+  ${DEST_DIR}/usr/sbin/* \
+  ${DEST_DIR}/lib/*
 set -e
 
-mkdir -p $OVERLAY_ROOTFS/usr
-mkdir -p $OVERLAY_ROOTFS/etc/autorun
+mkdir -p ${OVERLAY_ROOTFS}/usr
+mkdir -p ${OVERLAY_ROOTFS}/etc/autorun
 
 # With '--remove-destination' all possibly existing soft links in
 # '$OVERLAY_ROOTFS' will be overwritten correctly.
@@ -50,4 +50,4 @@ cp -r --remove-destination "$SRC_DIR/90_kbd.sh" \
 
 echo "Bundle '$BUNDLE_NAME' has been installed."
 
-cd $SRC_DIR
+cd ${SRC_DIR}
